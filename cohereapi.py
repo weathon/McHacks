2b29a280b5f39b809c4314dd3ee0910fc1a24398
ar3shoaib <ar.shoaib16@gmail.com>
2023-01-28 17:42:05 -0500

    Configured Cohere's API

---
+++ cohereapi.py
@@ -0,0 +1,14 @@
+import cohere
+
+co = cohere.Client('grqIE4jIdiHJz9llmB8SSzVHRyQ1TVtQMegrOx8u')
+
+def coheresapi (prompt):
+    response = co.generate(  
+        model='xlarge',  
+        prompt = prompt,  
+        max_tokens=40,  
+        temperature=0.6,  
+        stop_sequences=["--"])
+
+    drink_idea = response.generations[0].text
+    print (drink_idea)
