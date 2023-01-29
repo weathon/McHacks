e2617f38cc556de94c148a4d517e0a1630419dbb
Wayne <wguo6358@gmail.com>
2023-01-29 07:31:07 -0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -77,3 +77,19 @@ def cook():
         last_left_height = i[1]
     with open("cooked.csv","r") as f:
         return f.read()
+    
+import cohere
+@app.get("/cohere")
+def cohere:
+    co = ohere.Client('grqIE4jIdiHJz9llmB8SSzVHRyQ1TVtQMegrOx8u')
+    def coheresapi(prompt):
+        response = co.generate(  
+            model='xlarge',  
+            prompt = prompt,  
+            max_tokens=40,  
+            temperature=0.6,  
+            stop_sequences=["--"])
+
+        drink_idea = response.generations[0].text
+        return drink_idea
+    return coheresapi("Tell me a joke about drinks.")
